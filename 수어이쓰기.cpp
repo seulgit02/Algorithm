cc4dc1eceab0667b37e2cc1bf72379ad0938802d
seulgibaek <seulgi.baek@dgu.ac.kr>
2025-02-01 21:12:55 +0900

    [baekjoon] 수이어쓰기 오류 수정

--- 수어이쓰기.cpp
+++ 수어이쓰기.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
-#include <sstream>
+#include <cmath>
 using namespace std;
 int main(void) {
     int N, result = 0;
     cin >> N;
-    for (int i = 1; i <= N; i *= 10) {
-        result += N - i + 1;
-        cout << result << endl;
-    }
-    cout << result << endl;
-    // 120 -> 1, 100 ....
-    // result = 0
-    // result += (120 - 1 + 1) => 0 + 120
-    // result += (120 - 10 + 1) => 120 + 111 = 231
-    // result += (120 - 100 + 1) => 20 + 1 = 221 + 21 = 252
 
-    // 1200 -> 1, 100, 1000 ....
-    //result = 0
-    // result += (1200 - 1 + 1) => 1200
-    // result += (1200 - 100 + 1) => 1200 + 1101 => 2301
-    // result += (1200 - 1000 + 1) => 2301 + 201 => 2502
+    // N의 자릿수 구하기
+    int copyN = N;
+    int count = 0;
+    while (copyN >= 1) {
+        count++;
+        copyN = copyN/10;
+    }
+
+    if (N < 10) {
+        result = N;
+    }else {
+        result = 11;// 11자리부터 시작(13)
+
+        // 1억까지가 최대 범위이므로, i(자릿수)가 10보다 작을때까지 순회
+        for (int i = 2; i < 10; i++) {
+            if (count == i) {
+                result += i*(N-(pow(10, i-1)));
+                break;
+            }
+            // 해당 자릿수에 해당하는 최댓값
+            else {
+                result += i * (9 * pow(10, i - 1))+1;
+            }
+            
+        }
+
+    }
+    //if 2)10~99 -> (99-10+1)*2
+    //if 3) 100~999 ->(999-100+1)*3
+    cout << result << endl;
     return 0;
 };
