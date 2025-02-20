7e4147ff3ce282eb0b71d33658ddb9da980253bd
seulgibaek <seulgi.baek@dgu.ac.kr>
2025-02-20 19:10:04 +0900

    [backtracking] 스타트와 링크_14889

--- N과M_1(baekjoon_15649).cpp
+++ N과M_1(baekjoon_15649).cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 using namespace std;
-/*1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열*/
+
 int N, M;
 int arr[10];
-bool isused[10];
-
+int isused[10] = { 0, };
 void func(int k) {
     if (k == M) {
-        cout<<"k = " << M << "에 도달했습니다!"<<endl;
         for (int i = 0; i < M; i++) {
-            cout << arr[i] << " ";
+            cout << arr[i] << ' ';
         }
-        cout <<'\n';
-        return;
+        cout << "/n";
     }
     for (int i = 1; i <= N; i++) {
-        cout << "i = " << i << endl;
         if (!isused[i]) {
             arr[k] = i;
-            isused[i] = 1;
             func(k + 1);
+            // 다시 미사용 처리
             isused[i] = 0;
         }
-        cout << "isused[" << i << "] = " << isused[i] << ' ';
     }
-    cout << '\n';
 }
 
 int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
     cin >> N >> M;
     func(0);
     return 0;
@@ -55,3 +46,46 @@ int main(void) {
 // isused[2] = true
 // func(1)
 // ...
+
+/*1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열*/
+/*
+#include <iostream>
+using namespace std;
+int N, M;
+int arr[10];
+bool isused[10];
+
+void func(int k) {
+    if (k == M) {
+        cout << "finish." << endl;
+        for (int i = 0; i < M; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 1; i <= N; i++) {
+        if (!isused[i]) {
+            arr[k] = i;
+            cout << "arr[" << k << "]=" << arr[k] << ' ';
+            // 사용 처리
+            isused[i] = 1;
+            cout << "isused[" << i << "]=" << isused[i] << ' ';
+            func(k + 1);
+            // 길이가 M인 배열 생성 끝나면, 다시 미사용 상태로 초기화
+            isused[i] = 0;
+            cout << "isused[" << i << "]=" << isused[i] << ' ';
+        }
+    }
+    cout << '\n';
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    func(0);
+    return 0;
+}
+*/
---
+++ 스타트와링크(14889).cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#define INF 10000
+using namespace std;
+int result, N;
+int visited[20] = { 0, };
+int S[20][20];
+/*
+* start팀과 link팀 능력치 격차의 최소값 구하기
+ start 팀) visited[x] == 1인 원소
+ link 팀) visited[x] == 0인 원소
+*/
+void team(int a, int n) {
+    // 팀 배정인원이 다 찬 경우 팀의 능력치 계산
+    if (a == N / 2) {
+        int start = 0, link = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i] && visited[j]) {
+                    start += S[i][j];
+                }
+                else if (!visited[i] && !visited[j]) {
+                    link += S[i][j];
+                }
+            }
+        }
+        result = min(result, abs(start - link));
+        return;
+    }
+    // 팀 배정인원 추가 할당
+    else {
+        for (int i = n; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = 1;
+                team(a + 1, i + 1);
+                visited[i] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> S[i][j];
+        }
+
+    }
+    result = INF;
+    team(0, 0);
+    cout << result;
+    return 0;
+}
